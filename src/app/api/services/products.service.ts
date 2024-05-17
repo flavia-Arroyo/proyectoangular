cbe3c16ff5bd67be97e2edf35c9b81f9fceb6cf3
flavia-Arroyo <flavia.e.arroyo@gmail.com>
2024-05-17 17:19:15 -0300

    agrego boostrap grid

--- src/app/api/services/products.service.ts
+++ src/app/api/services/products.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-
+  private http=inject(HttpClient);
   constructor() { }
+  getAll(){
+    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+  }
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../api/services/products.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { Product } from '../../api/model/product';
 
 @Component({
   selector: 'app-pages-products',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products: Product[]=[]
+  constructor(private productService:ProductsService){
+    this.productService.getAll().subscribe({
+      next:data=>{
+        this.products = data;
+      },
+      error:err=>{
+        console.log(err)
+      }
+    });
+  }
 
 }
